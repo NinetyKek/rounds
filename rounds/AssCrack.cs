6bcf831838312bd232341249fbd0e0eeec597bbd
_90_ <81204025+NinetyKek@users.noreply.github.com>
2022-04-30 23:35:07 -0400

    Initial commit

---
+++ rounds/AssCrack.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx;
+using UnboundLib;
+using UnboundLib.Cards;
+using AssCrack.Cards;
+using HarmonyLib;
+using CardChoiceSpawnUniqueCardPatch.CustomCategories;
+
+namespace AssCrack
+{
+    // These are the mods required for our mod to work
+    [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.moddingutils", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.cardchoicespawnuniquecardpatch", BepInDependency.DependencyFlags.HardDependency)]
+    // Declares our mod to Bepin
+    [BepInPlugin(ModId, ModName, Version)]
+    // The game our mod is associated with
+    [BepInProcess("Rounds.exe")]
+    public class AssCrack : BaseUnityPlugin
+    {
+        private const string ModId = "com.Luna.rounds.AssCrack";
+        private const string ModName = "Ass Crack";
+        public const string Version = "1.0.0"; // What version are we on (major.minor.patch)?
+        public const string ModInitials = "AC";
+        public static AssCrack instance { get; private set; }
+
+        void Awake()
+        {
+            // Use this to call any harmony patch files your mod may have
+            var harmony = new Harmony(ModId);
+            harmony.PatchAll();
+        }
+        void Start()
+        {
+            instance = this;
+            CustomCard.BuildCard<EvasiveManeuvers>();
+        }
+    }
+}
